6ca4995d956795d7571e70c27d2ae92cb22a79b9
Ahmad Muzaky <2410631170056@student.unsika.ac.id>
2026-09-09 10:27:14 +0700

    Update Praktikum 2

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcomeee');
+Route::get('/about', function () {
+    return 'Selamat Datang di Barokah Mart!!<br>
+            Barokah Mart merupakan sebuah toko kelontong yang sudah berdiri selama 2300 tahun.<br>
+            Didirikan oleh Haji Isam(il) bin mail, pada tahun 360-SM Bersama Alexander the Great.<br>
+            Pilihan produknya banyak, dari yang receh sampai yang mahal, pilih aja sendiri.';
 });
 
 use App\Http\Controllers\DashboardController;
