843df24e949fbffa7cbf2f165a58e99542230f35
Ahmad Muzaky <2410631170056@student.unsika.ac.id>
2026-09-09 12:43:22 +0700

    Update Praktikum 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,14 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    
+    ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'auth' => Authenticate::class,
+            'role' => \App\Http\Middleware\CheckRole::class,
         ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+    
